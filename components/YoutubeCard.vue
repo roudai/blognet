7a88872b3181276f1e17c8b936404d42a5d408ea
roudai <me@roudai.net>
2022-11-20 00:00:50 +0900

    388記事作成

---
+++ components/YoutubeCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="column is-8 is-offset-2">
+    <youtube :video-id="id" class="youtube"></youtube>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.youtube {
+  width: 100%;
+  max-width: 640px;
+  margin: 1rem;
+}
+</style>
