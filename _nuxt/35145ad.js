03bc456899bd1b61ea235bf08ead2373e53ce6dd
roudai <roudai@users.noreply.github.com>
2022-11-19 15:02:04 +0000

    deploy: 7a88872b3181276f1e17c8b936404d42a5d408ea

---
+++ _nuxt/35145ad.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{293:function(n,t,e){"use strict";e.r(t);var c=e(23),component=Object(c.a)({},(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("h1",[this._v("ルービックキューブ世界記録4.90秒の凄さを一般人にも分かりやすく解説")])])}],!1,null,null,null);t.default=component.exports}}]);
