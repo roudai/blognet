c3f98da7a6410ccd18fa4ee80a88d9fc66851e99
roudai <me@roudai.net>
2022-11-19 23:24:43 +0900

    記事388追加途中

--- pages/post-388.vue
+++ pages/post-388.vue
@@ -1,7 +1,152 @@
 <template>
   <div>
-    <h1>
-      2016年にルービックキューブ公式大会で起きた懲罰・重大インシデントまとめ
-    </h1>
+    <h1>2016年にルービックキューブ公式大会で起きた懲罰・重大インシデントまとめ</h1>
+    <p>
+      ※この記事は、<a href="https://adventar.org/calendars/1361" target="_blank">Speedcubing Advent Calendar 2016</a
+      >の企画として執筆したものです。
+    </p>
+    <p>
+      現在ルービックキューブの大会は世界中で行われていますが、規模が大きくなるにつれて、トラブル（インシデント）も多くなります。
+    </p>
+    <p>
+      世界キューブ協会(World Cube Association : WCA)では、大会でのインシデントが報告された場合、 WCA懲罰委員会（WCA
+      Disciplinary Committee：WDC）でその内容を審査し、対象者の処分等を決定します。
+    </p>
+    <p>
+      そして重大な内容についてはWCA
+      Organisationのフォーラムで報告がされるため、一般の会員もその内容を知ることができます。<br />
+      <a href="https://www.worldcubeassociation.org/archive/forums/9" target="_blank"
+        >https://www.worldcubeassociation.org/archive/forums/9</a
+      >
+    </p>
+    <p>
+      この記事では、2016年にWCA Organisationフォーラムで報告された懲罰・重大インシデントについてまとめたいと思います。
+    </p>
+
+    <h2>競技者の不正によるインシデント</h2>
+    <p>まずは、競技者が大会中に行った不正によるインシデントです。</p>
+
+    <h3>目隠し競技中の不正</h3>
+    <p>最も多いインシデントは、目隠し競技での不正です。2016年だけで5件も報告されています</p>
+    <p>
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2008" target="_blank"
+        >Incident in 3x3x3 Blindfolded at Great Indian Cubeathon 2016</a
+      ><br />
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2013" target="_blank"
+        >Incident at Blind and Whatnot 2016</a
+      ><br />
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2021" target="_blank"
+        >Incidents in blindfolded events at Spanish competitions</a
+      ><br />
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2025" target="_blank">Incident at SCMU 2016</a
+      ><br />
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2026" target="_blank"
+        >Incident at National Cubing Region 2016</a
+      ><br />
+    </p>
+    <p>
+      目隠し競技での不正については、2007年～2008年にかけて数多くの世界記録を持っていたハンガリーのMátyás
+      Kutiが目隠しの下から覗き見るという行為をしていたことが発覚
+      （3年間の出場停止処分）して以来、目隠しに加えて不透明な板を用いることが義務付けられましたが、それでもなお不正はなくならない、
+      というより競技者の増加に伴ってますます増えているように思います。
+    </p>
+    <p>また、2016年に報告されている5件の不正がすべて18歳未満（氏名非公開）というのも注目すべき点かもしれません。</p>
+    <p>この不正に対する処分は、1年間の公式大会出場停止が通例となっているようです。</p>
+
+    <h3>最少手数競技中の不正</h3>
+    <p>
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2009" target="_blank"
+        >Incident in 3x3x3 Fewest Moves at Great Indian Cubeathon 2016</a
+      >
+    </p>
+    <p>
+      2016年3月5日～6日にベルギーで行われたZonhoven Open
+      2016にて、18歳未満の競技者が最少手数の試技中にスマートフォンを操作して手順を盗み見しているのが発見されました。試験でのカンニングと同じですね。
+    </p>
+    <p>
+      北京でのアジア大会では、こういった不正を防ぐために事前にスマートフォンを回収されました。
+      競技者はこの不正で半年間の公式大会出場停止になっています。
+    </p>
+
+    <h3>インスペクション中の不正</h3>
+    <p>
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2024" target="_blank"
+        >Incident at Iran Grand Prix 2016</a
+      >
+    </p>
+    <p>
+      2016年4月14日～15日にイランで行われたIran Grand Prix
+      2016にて、複数の競技でインスペクション中にパズルを動かしてズルをしていたことが発覚しました。
+    </p>
+    <p>
+      ジャッジがちゃんと見ておけよという感じではありますが、もちろんこれは不正です。
+      競技者はこの不正で1年間の公式大会出場停止になっています。
+    </p>
+
+    <h3>スクランブルに関する不正 その1</h3>
+    <p>
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2022" target="_blank"
+        >Incident at Los Lagos Open 2016</a
+      >
+    </p>
+    <p>
+      2016年4月2日～3日にチリで行われたLos Lagos Open 2016にて、2x2の競技中にジャッジをしていたManuel
+      Torresが、知り合いの競技者にわざと簡単なスクランブル状態になったキューブを渡しました。
+    </p>
+    <p>
+      そのソルブではとても早い記録がでましたが、スクランブルが本来のものと違っていたことはすぐにバレてしまったようです。
+    </p>
+    <p>彼はこの不正で3ヵ月の公式大会出場停止になっています。</p>
+
+    <h3>スクランブルに関する不正 その2</h3>
+    <p>
+      <a href="https://www.worldcubeassociation.org/archive/forum_topics/2012" target="_blank"
+        >Incident at Manhasset 2015 and Williams Winter 2016</a
+      >
+    </p>
+    <p>
+      2015年11月21日のManhasset 2015、2016年2月13日のWilliams Winter
+      2016にて、ある競技者がスクランブラをしている際に、意図的にスクランブルを簡単なものにしていたという事案がありました。
+    </p>
+    <p>その一例が、元世界記録保持者のCollin Burnsが行ったこの試技です。</p>
+    <p>
+      <a href="https://www.youtube.com/watch?v=m_NsanT6WsU" target="_blank">5.21 Misscramble</a>
+    </p>
+    <p>
+      5.21秒というかつて持っていた世界記録を超える記録ですが、解析をすると、2手でF2L 2つ目まで、5手でF2L
+      3つ目まで終わるという、とんでもないスクランブルであることが分かります。
+    </p>
+    <p>
+      （参考：
+      <a
+        href="https://www.speedsolving.com/threads/collin-burns-5-21-3x3-single-miss-scrambled-and-replaced.55946/"
+        target="_blank"
+        >Collin Burns - 5.21 3x3 Single (miss-scrambled and replaced)</a
+      >
+      ）
+    </p>
+
+    <h2>運営不備、ルール不備によるインシデント</h2>
+    <h3>Square-1スクランブルに関するインシデント</h3>
+
+    <h3>Square-1試技中のインシデント</h3>
+
+    <h3>最少手数解法に関するインシデント</h3>
+
+    <h2>その他のインシデント</h2>
+    <h3>ルービックキューブというものを理解していなかったことによるインシデント</h3>
+
+    <h2>まとめ</h2>
+    <p>以上、WCA Organisationフォーラムで報告された懲罰・重大インシデントのまとめでした。</p>
+    <p>
+      日本で行われる大会ではこのような重大インシデントが起きることはとても少ないため、あまりこういった内容について敏感ではないかもしれません。しかし特に18歳未満の子供が起こすような不正については、いつ日本で起きてもおかしくないものだと思います（tribox
+      Contestでさえ不正や荒らしが発生しているような状況ですから）。
+    </p>
+    <p>
+      大会参加への間口が広がるのはとても良いことですが、人が多くなるともちろんこういったトラブル・インシデントが起こる可能性も高くなりますし、これからの日本でのルービックキューブの発展を考えるうえで課題はたくさんあるのではないでしょうか。
+    </p>
+    <p>
+      最後に、WCA懲罰委員会では、日本のokayamaさんが最近までメンバーとして活動されていました。今回この記事を書くにおいても一部お話を聞かせていただき、ここに感謝の意を表します。
+    </p>
   </div>
 </template>
