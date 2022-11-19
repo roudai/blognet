7007dca7bf9c3e2c5cc703fe6a96889d0ab1ba31
roudai <roudai@users.noreply.github.com>
2022-11-19 13:48:56 +0000

    deploy: a73cd0a8ce26b7d60b94feb87d64b9139f3d6af8

--- _nuxt/64b9676.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{266:function(n,t,e){"use strict";e.r(t);var c=e(23),component=Object(c.a)({},(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("h1",[this._v("\n    2016年にルービックキューブ公式大会で起きた懲罰・重大インシデントまとめ\n  ")])])}],!1,null,null,null);t.default=component.exports}}]);
