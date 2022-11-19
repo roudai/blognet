ed0870d96465f330ab67d3711f07144e78121c12
roudai <me@roudai.net>
2022-11-20 00:33:20 +0900

    タイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blognet',
+    title: 'ブログ版ろーだいねっと',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'ブログ版ろーだいねっとアーカイブ' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
