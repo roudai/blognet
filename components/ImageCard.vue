cac8bd29e96018a952b50efa57b9c42dca46bfbf
roudai <me@roudai.net>
2022-11-21 23:58:54 +0900

    画像表示をコンポーネント化

---
+++ components/ImageCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="columns ml-0 mr-0">
+    <img :src="imagePath()" class="column is-8 is-offset-2" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    imagePath() {
+      return require(`@/assets/${this.name}.webp`)
+    },
+  },
+}
+</script>
